4cd1eec45b5d151bdabe87cd38062aae07b7b804
Oleh Ivanytskyi <89008107+ivanytskyi2407@users.noreply.github.com>
2023-04-10 13:00:44 +0300

    refactoring

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,27 +1,37 @@
-// import React from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions ';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 import { useState } from 'react';
 const Counter = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const buttons = ['good', 'neutral', 'bad'];
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const { good, neutral, bad } = feedback;
   const totalFeedback = good + neutral + bad;
   const percentPositivFeedback = Math.round((100 / totalFeedback) * good);
 
   const onLeaveFeedback = e => {
     switch (e) {
       case 'good':
-        return setGood(prevState => prevState + 1);
+        return setFeedback(prevState => ({
+          ...prevState,
+          good: prevState.good + 1,
+        }));
 
       case 'neutral':
-        return setNeutral(prevState => prevState + 1);
+        return setFeedback(prevState => ({
+          ...prevState,
+          neutral: prevState.neutral + 1,
+        }));
 
       case 'bad':
-        return setBad(prevState => prevState + 1);
+        return setFeedback(prevState => ({
+          ...prevState,
+          bad: prevState.bad + 1,
+        }));
       default:
         return;
     }
@@ -29,7 +39,10 @@ const Counter = () => {
   return (
     <div>
       <Section title="Please leave feedback">
-        <FeedbackOptions options={buttons} onLeaveFeedback={onLeaveFeedback} />
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={onLeaveFeedback}
+        />
       </Section>
 
       <Section title="Statistics">
@@ -48,62 +61,4 @@ const Counter = () => {
     </div>
   );
 };
-// class Counter extends React.Component {
-//   static totalFeedback = 0;
-//   static percentPositivFeedback = 0;
-
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   onLeaveFeedback = name => {
-//     this.setState(prevState => {
-//       return {
-//         [name]: prevState[name] + 1,
-//       };
-//     });
-//   };
-
-//   countTotalFeedback() {
-//     const { good, neutral, bad } = this.state;
-//     this.totalFeedback = good + neutral + bad;
-//   }
-//   countPositiveFeedbackPercentage() {
-//     this.percentPositivFeedback = Math.round(
-//       (100 / this.totalFeedback) * this.state.good
-//     );
-//   }
-
-//   render() {
-//     const buttons = Object.keys(this.state);
-//     this.countTotalFeedback();
-//     this.countPositiveFeedbackPercentage();
-//     return (
-//       <div>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={buttons}
-//             onLeaveFeedback={this.onLeaveFeedback}
-//           />
-//         </Section>
-
-//         <Section title="Statistics">
-//           {this.totalFeedback === 0 ? (
-//             <Notification message="There is no feedback" />
-//           ) : (
-//             <Statistics
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={this.totalFeedback}
-//               positivePercentage={this.percentPositivFeedback}
-//             />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
 export default Counter;
